2620e1dcf61085c36dfbe40730362ed990dd2cb5
Mikey Zhong <mzhong21@gmail.com>
2024-11-22 23:46:13 -0500

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,78 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <section className="max-w-3xl mx-auto p-4">
+      <section>
+        <section>
+          <h4>Hi!</h4>
+          <p>
+            I&apos;m Mikey, I live in NYC, and I&apos;m currently heads down
+            working on a project called sketches, which is my attempt to explain
+            ML concepts with more pictures and fewer words.
+          </p>
+        </section>
+        <section>
+          <h4>A few dreams I'd like to manifest:</h4>
+          <ul>
+            <li>Accessible education for anyone.</li>
+            <li>
+              I'd love to host weekly pasta nights of 8-20 people where I don't
+              know everyone.
+            </li>
+            <li>
+              Live in a 4-apartment townhouse with friends who like to make
+              things, working on our projects.
+            </li>
+            <li>Have a little grapefruit patch in a mini-greenhouse.</li>
+            <li>
+              <a
+                href="https://www.instagram.com/p/CiftimKJdiz/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Have five little shacks in NYC, Seattle, Porto, Tokyo, and
+                Sydney.
+              </a>
+            </li>
+          </ul>
+        </section>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <h4>A few things I geek out to:</h4>
+        <ul>
+          <li>Anything design-related, but particularly typography.</li>
+          <li>
+            Anything ML-related, particularly scene reconstruction and
+            self-supervised learning problems in computer vision.
+          </li>
+          <li>
+            Grapefruit juice.{" "}
+            <a
+              href="https://www.instagram.com/grapefruitreviews"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              I have an IG
+            </a>{" "}
+            where I write reviews on different grapefruit juice I try and books
+            I read.
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h4>A few ways to say hi...</h4>
+        <span>
+          <ul>
+            <li>
+              <a href="https://www.instagram.com/mikeyzhong21/">IG</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/michael-zhong-b0b58938/">
+                LinkedIn
+              </a>
+            </li>
+          </ul>
+        </span>
+      </section>
+      <p>At some point I hope to add more to this website.</p>
+    </section>
   );
 }
